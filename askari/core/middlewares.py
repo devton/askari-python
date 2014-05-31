33a718c4cbf023404d53bcf79a0ef8f5a6e073eb
Antônio Roberto Silva <forevertonny@gmail.com>
2014-05-31 17:36:00 -0300

    create a proxyuser with relation methods for request.user with a middleware

---
+++ askari/core/middlewares.py
@@ -0,0 +1,7 @@
+from .models import ProxyUser
+
+
+class ProxyUserMiddleware():
+    def process_request(self, request):
+        if hasattr(request, 'user') and request.user.is_authenticated():
+            request.user.__class__ = ProxyUser
