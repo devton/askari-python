2e231f29e2720410670ba974340941e5f08395f3
Antônio Roberto Silva <forevertonny@gmail.com>
2014-06-01 04:00:20 -0300

    remove this core/forms.py and adjust identation

--- askari/core/views.py
+++ askari/core/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 class LoginRequiredViewMixin(object):
-
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
-       return super(LoginRequiredViewMixin, self).dispatch(*args, **kwargs)
+        return super(LoginRequiredViewMixin, self).dispatch(*args, **kwargs)
