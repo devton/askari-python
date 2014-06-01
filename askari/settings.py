f769c1740d7b563e4eb61691aa736154a7e101dd
Antônio Roberto Silva <forevertonny@gmail.com>
2014-06-01 02:51:09 -0300

    basic bootstrap3 login screen

--- askari/settings.py
+++ askari/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'bootstrap3',
 )
 
 # A sample logging configuration. The only tangible logging
