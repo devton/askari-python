7118adfcfb767f82ca328e7082d687d7563cf5b9
Antônio Roberto Silva <forevertonny@gmail.com>
2014-07-16 13:26:13 -0300

    made tests for dataclips slug

---
+++ askari/dataclips/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from ..databases.models import Database
+from .models import Clip
+
+
+class ClipTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create(username=u'test', password='test')
+        self.database = Database.objects.create(user=self.user)
+        self.clip = Clip.objects.create(name='foo', database=self.database)
+
+    def test_slug_field_filled(self):
+        self.assertRegexpMatches(self.clip.slug, r'^\w{32}$')
