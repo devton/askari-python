676528d539c393805bc67c7e11ccc54f02626ebe
Antônio Roberto Silva <forevertonny@gmail.com>
2014-07-19 22:52:08 -0300

    test new and delete view of database

--- askari/databases/test_views.py
+++ askari/databases/test_views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
@@ -15,19 +16,55 @@ class DatabaseViewTest(TestCase):
     def do_login(self):
         self.client.login(username='test', password='123456')
 
-    def list_request(self):
+    def list_view_request(self):
         self.url = reverse('databases:list')
         self.response = self.client.get(self.url)
 
+    def new_view_request(self):
+        self.url = reverse('databases:new')
+        self.response = self.client.get(self.url)
+
+    def delete_view_request(self):
+        self.url = reverse('databases:delete', kwargs={'pk': self.database.pk})
+        self.response = self.client.delete(self.url)
+
+    def test_new_view_without_login(self):
+        self.new_view_request()
+
+        self.assertEqual(self.response.status_code, 302)
+        self.assertRedirects(self.response, "/login/?next={}".format(self.url))
+
+    def test_new_view(self):
+        self.do_login()
+        self.new_view_request()
+
+        self.assertEqual(self.response.status_code, 200)
+
     def test_list_view_without_login(self):
-        self.list_request()
+        self.list_view_request()
 
         self.assertEqual(self.response.status_code, 302)
         self.assertRedirects(self.response, "/login/?next={}".format(self.url))
 
     def test_list_view(self):
         self.do_login()
-        self.list_request()
+        self.list_view_request()
 
         self.assertEqual(self.response.status_code, 200)
         self.assertIn(self.database.name, self.response.rendered_content)
+
+    def test_delete_view_without_login(self):
+        self.delete_view_request()
+
+        self.assertEqual(self.response.status_code, 302)
+        self.assertRedirects(self.response, "/login/?next={}".format(self.url))
+
+    def test_delete_view(self):
+        self.do_login()
+        self.delete_view_request()
+
+        self.assertEqual(self.response.status_code, 302)
+        self.assertRedirects(self.response, "/databases/")
+
+        self.list_view_request()
+        self.assertNotIn(self.database.name, self.response.rendered_content)
